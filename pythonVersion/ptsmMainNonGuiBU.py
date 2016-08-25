3377c71d626838d11b3f6ae31fd762dad2240092
jiajun yang <eric1986_chn@yahoo.com>
2016-08-25 17:09:25 +0200

    Can click and make sound. add init paras. exp_table is generated in genData button.

--- pythonVersion/ptsmMainNonGuiBU.py
+++ pythonVersion/ptsmMainNonGuiBU.py
@@ -113,10 +113,14 @@ N, dim = data.shape[0], data.shape[1]
 
 exp_resolution = 1000000 # Resolution for lookup table
 delta_dis = np.linspace(0.0, dim, num = exp_resolution) # Range of distance difference.
+# m_comp = 0.001 # mass compensation
 norm_max = dim
-m_comp, dt, resistant, sigma = 0.02, 0.005 , 0.999, 0.15 # init
+sigma = 0.1
+
 exp_table = Exptable().updateExpTable(sigma, delta_dis)
+
 fig, ax = plt.subplots()
+m_comp, dt, resistant, sigma = 0.02, 0.005 , 0.999, 0.15 # init
 max_sigma = np.log(dim)/2.5 * 0.5 + 0.1
 t = 1.0 # Time in second per piece
 blockSize = 5000   # Buffer size for trajectory
@@ -194,133 +198,3 @@ sresistant.on_changed(sliderUpdate)
 resultWindow = plt.figure(2, figsize=(8, 8))
 fig.canvas.mpl_connect('pick_event', on_pick)
 # plt.show()
-
-
-class Ptsgui(QtGui.QMainWindow):
-
-    def __init__(self):
-        super(Ptsgui, self).__init__()
-
-        self.initUI()
-        self.data, self.pos = np.zeros(2), np.zeros(2)
-        self.vel , self.t = 0, 1.
-        self.exp_table = np.zeros(1) # Initialise the exp table
-        self.norm_max = 0
-
-        # TODO These parameter should become sliders
-        self.m_comp, self.dt, self.resistant, self.sigma = 0.02, 0.005, 0.999, 0.15  # init
-
-        self.t = 1.0  # Time in second per piece, TODO a para
-        self.blockSize = 5000  # Buffer size for trajectory TODO a para
-        self.audioVecSize = int(self.t * self.blockSize)  # I define that 5000 steps will return 1 second of audio
-
-
-    def datafigon_pick(self, event):
-        ind = np.array(event.ind)  # Get the index of the clicked data
-        self.pos = np.array(data[ind[0], :])
-        self.vel = np.random.rand(dim) - 0.5
-
-        trj, junk, forceSound = Trajectory.PTSM(self.pos, self.data, self.vel, self.exp_table, exp_resolution, \
-                                                self.norm_max, sigma=self.sigma, dt=self.dt, r=self.resistant, \
-                                                Nsamp=self.audioVecSize, compensation=self.m_comp)
-        velSound = trj[:, 0] / np.max(np.absolute(trj[:, 0])) * windowing
-        velSound = DSP().butter_lowpass_filter(velSound, 2000.0, fs, 6)  # 6th order
-        # forceSound = forceSound / np.max(np.absolute(forceSound))
-        stopevent = threading.Event()
-        producer = threading.Thread(name="Compute audio signal", target=proc, args=[fifo, stopevent, velSound])
-        producer.start()
-
-
-
-    def sendData(self):
-        print "Send data"
-
-    def genData(self):
-        self.data = DataGen().datagen(4, 3, sigma=0.4, minnr=100, maxnr=300)
-        self.N, self.dim = self.data.shape[0], self.data.shape[1]
-        self.norm_max = dim
-        exp_resolution = 1000000  # Resolution for lookup table
-        delta_dis = np.linspace(0.0, self.dim, num=exp_resolution)  # Range of distance difference.
-        self.exp_table = Exptable().updateExpTable(self.sigma, delta_dis)
-        self.dataFigAx.scatter(data[:, 0], data[:, 1], c="blue", picker=2, s=[50] * N)
-        self.dataFigCanvas.draw()
-        print self.data
-
-    def initUI(self):
-        self.statusBar().showMessage('Ready')  # Tell user to wait while sending data
-        self.setGeometry(50, 50, 1050, 650)
-        self.setWindowTitle('PTS')
-
-        mainlayout = QtGui.QVBoxLayout()
-
-        self.widget = QtGui.QWidget()
-        self.widget.setLayout(mainlayout)
-        self.setCentralWidget(self.widget)  # Set main layout to layout of widget!!!
-
-        # Sub layout 1
-        plotBox = QtGui.QHBoxLayout()
-        cltBox = QtGui.QHBoxLayout()
-        cltBox.setSpacing(30)
-        mainlayout.addLayout(plotBox)
-        mainlayout.addLayout(cltBox)
-
-        genDataButton = QtGui.QPushButton('Generate Data', self)
-        genDataButton.setToolTip('This button randomise a \
-            set of data based on the input dimension and cluster amount.')
-        genDataButton.resize(genDataButton.sizeHint())
-        # genDataButton.move(50, 550)
-        genDataButton.clicked.connect(self.genData)
-        cltBox.addWidget(genDataButton)
-        sendDataButton = QtGui.QPushButton("Send Data", self)
-        sendDataButton.resize(genDataButton.sizeHint())
-        # sendDataButton.move(200, 550)
-        sendDataButton.clicked.connect(self.sendData)
-        cltBox.addWidget(sendDataButton)
-
-        self.dataFig = plt.figure()
-        self.dataFigCanvas = FigureCanvas(self.dataFig)
-        self.dataFigCanvas.mpl_connect('pick_event', self.datafigon_pick)
-        self.dataFigAx = self.dataFig.add_subplot(111)
-        self.dataFigAx.set_xlim([-0.5, 0.5])
-        self.dataFigAx.set_ylim([-0.5, 0.5])
-        self.dataFigToolbar = NavigationToolbar(self.dataFigCanvas,self)
-        plotBoxL = QtGui.QVBoxLayout()
-        plotBoxL.setSpacing(5)
-        plotBoxL.addWidget(self.dataFigCanvas)
-        plotBoxL.addWidget(self.dataFigToolbar)
-
-        self.trjFig = plt.figure()
-        self.trjFigCanvas = FigureCanvas(self.trjFig)
-        self.trjFigAx = self.trjFig.add_subplot(111)
-        self.trjFigAx.set_xlim([-0.5, 0.5])
-        self.trjFigAx.set_ylim([-0.5, 0.5])
-        self.trjFigToolbar = NavigationToolbar(self.trjFigCanvas,self)
-        plotBoxR = QtGui.QVBoxLayout()
-        plotBoxR.setSpacing(5)
-        plotBoxR.addWidget(self.trjFigCanvas)
-        plotBoxR.addWidget(self.trjFigToolbar)
-
-        plotBox.addLayout(plotBoxL)
-        plotBox.addLayout(plotBoxR)
-
-        self.show()
-
-
-    def closeEvent(self, event):
-		reply = QtGui.QMessageBox.question(self, 'Message',
-            "Are you sure to quit?", QtGui.QMessageBox.Yes |
-            QtGui.QMessageBox.No, QtGui.QMessageBox.Yes)
-
-		if reply == QtGui.QMessageBox.Yes:
-			event.accept()
-		else:
-			event.ignore()
-
-
-def main():
-	app = QtGui.QApplication(sys.argv)
-	ptsgui = Ptsgui()
-	sys.exit(app.exec_())
-
-if __name__ == '__main__':
-	main()
