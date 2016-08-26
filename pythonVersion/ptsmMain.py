19fb0c32610dfbec3b0700e9d85cebd242f1ec3c
jiajun yang <eric1986_chn@yahoo.com>
2016-08-26 17:06:36 +0200

    Link all inputs, optimised changedValue method

--- pythonVersion/ptsmMain.py
+++ pythonVersion/ptsmMain.py
@@ -164,68 +164,66 @@ class Ptsgui(QtGui.QMainWindow):
         sendDataButton.setToolTip('Send the first 2 columns of data to Android device for plotting.')
         #----------------------
 
-        #TODO need a display box.
+        #sigma
         sigmaTitle = QtGui.QLabel('Sigma')
         sigmaTitle.resize(sigmaTitle.sizeHint())
-        sigmaSlider = QtGui.QSlider(QtCore.Qt.Horizontal, self)
-        sigmaSlider.setToolTip('Large sigma results in 1 global minimum while small sigma results in local minimum '
+        self.sigmaSlider = QtGui.QSlider(QtCore.Qt.Horizontal, self)
+        self.sigmaSlider.setToolTip('Large sigma results in 1 global minimum while small sigma results in local minimum '
                                'in each data point')
 
-        sigmaSlider.setRange(0, 100)
-        sigmaSlider.setValue(50)
-        sigmaSlider.valueChanged[int].connect(self.sigmaChangeValue)
+        self.sigmaSlider.setRange(0, 100)
+        self.sigmaSlider.setValue(50)
+        self.sigmaSlider.valueChanged[int].connect(self.changeValue)
         self.sigmaDisplay = QtGui.QLineEdit()
         self.sigmaDisplay.setFixedSize(50, 20)
 
-
+        #dt
         dt_init = 0.005
         dtTitle = QtGui.QLabel('dt')
-        dtSlider = QtGui.QSlider(QtCore.Qt.Horizontal, self)
-        dtSlider.setToolTip('A larger dt will lead to a bigger distance change at each iteration.')
-        dtSlider.setRange(0, 100)
+        self.dtSlider = QtGui.QSlider(QtCore.Qt.Horizontal, self)
+        self.dtSlider.setToolTip('A larger dt will lead to a bigger distance change at each iteration.')
+        self.dtSlider.setRange(0, 100)
         temp = linlinInvert(dt_init, 0, 100, 0.001, 0.01)
-
-        dtSlider.valueChanged[int].connect(self.dtChangeValue)
-        dtSlider.setValue(temp)
-
+        self.dtSlider.setValue(temp)
         self.dtDisplay = QtGui.QLineEdit()
         self.dtDisplay.setFixedSize(50, 20)
         self.dtDisplay.setText(str(dt_init))
+        self.dtSlider.valueChanged[int].connect(self.changeValue)
 
+        #r
         rTitle = QtGui.QLabel('r')
-        rSlider = QtGui.QSlider(QtCore.Qt.Horizontal, self)
-        rSlider.setRange(0, 100)
+        self.rSlider = QtGui.QSlider(QtCore.Qt.Horizontal, self)
+        self.rSlider.setRange(0, 100)
         r_init = 0.999
         temp = linlinInvert(r_init, 0, 100, 0.99, 1.0)
         # print "rInit " + str(linlin(temp,0, 100, 0.99, 1.0))
-        rSlider.setValue(temp)
-        rSlider.valueChanged[int].connect(self.rChangeValue)
-        rSlider.setToolTip('r is the recipricate of friction. at r = 1, the particle will have no velocity lost.')
+        self.rSlider.setValue(temp)
+        self.rSlider.setToolTip('r is the recipricate of friction. at r = 1, the particle will have no velocity lost.')
         self.rDisplay = QtGui.QLineEdit()
         self.rDisplay.setFixedSize(50, 20)
         self.rDisplay.setText(str(r_init))
+        self.rSlider.valueChanged[int].connect(self.changeValue)
         #----------------------#
 
 
 
 
-
         # left pannel.
-
-
         dimTitle = QtGui.QLabel('Dimension')
         dimTitle.setFixedSize(70, 20)
         self.dimDisplay = QtGui.QSpinBox()
-        # self.dimDisplay.setValidator(QtGui.QIntValidator())
+
         self.dimDisplay.setRange(2, 1000)
         self.dimDisplay.setValue(3)
         self.dimDisplay.setFixedSize(50, 20)
         self.dimDisplay.resize(self.dimDisplay.sizeHint())
         self.dimDisplay.setToolTip('Choose the dimensionality of the data. Type = int, Min = 2, Max = 1000')
+        self.dimDisplay.valueChanged[int].connect(self.changeValue)
+        # self.rSlider.valueChanged[int].connect(self.changeValue)
 
 
         ncTitle = QtGui.QLabel('Clusters')
-        # ncTitle.resize(dimTitle.sizeHint())
+
         ncTitle.setFixedSize(70, 20)
         self.ncDisplay = QtGui.QSpinBox()
         self.ncDisplay.setValue(4)
@@ -233,10 +231,10 @@ class Ptsgui(QtGui.QMainWindow):
         self.ncDisplay.setFixedSize(50, 20)
         self.ncDisplay.resize(self.ncDisplay.sizeHint())
         self.ncDisplay.setToolTip('Choose the number of clusters for the simulated data. Type = int, Min = 1, Max = 1000')
+        self.ncDisplay.valueChanged[int].connect(self.changeValue)
 
         nrminTitle = QtGui.QLabel('Nr_min')
         nrminTitle.setFixedSize(70, 20)
-        # nrminTitle.resize(dimTitle.sizeHint())
         self.nrminDisplay = QtGui.QSpinBox()
         self.nrminDisplay.setRange(10, 300)
         self.nrminDisplay.setValue(50)
@@ -244,10 +242,10 @@ class Ptsgui(QtGui.QMainWindow):
         self.nrminDisplay.resize(self.nrminDisplay.sizeHint())
         self.nrminDisplay.setToolTip('Choose the minimum number of row for each clusters. 10~300, type: int')
         # Use the changed value box to detect is nr_max < mr_min
+        self.nrminDisplay.valueChanged[int].connect(self.changeValue)
 
         nrmaxTitle = QtGui.QLabel('Nr_max')
         nrmaxTitle.setFixedSize(50, 20)
-        # nrmaxTitle.resize(dimTitle.sizeHint())
         self.nrmaxDisplay = QtGui.QSpinBox()
         self.nrmaxDisplay.setRange(100, 600)
         self.nrmaxDisplay.setValue(200)
@@ -255,7 +253,7 @@ class Ptsgui(QtGui.QMainWindow):
         self.nrmaxDisplay.resize(self.nrmaxDisplay.sizeHint())
         self.nrmaxDisplay.setToolTip('Choose the maximum number of row for each clusters. 100~600, type: int')
         # Use the changed value box to detect is nr_max < mr_min
-
+        self.nrmaxDisplay.valueChanged[int].connect(self.changeValue)
 
 
         ipTitle = QtGui.QLabel('Tablet IP')
@@ -265,6 +263,8 @@ class Ptsgui(QtGui.QMainWindow):
         self.ipDisplay.setText(HOMEIP)
         # self.ipDisplay.resize(self.dimDisplay.sizeHint())
         self.ipDisplay.setFixedSize(106, 20)
+        self.ipDisplay.returnPressed.connect(self.ipChangeValue)
+        # self.ipDisplay.valueChanged[str].connect(self.changeValue)
 
 
         cltLeftBox = QtGui.QGridLayout()
@@ -281,13 +281,13 @@ class Ptsgui(QtGui.QMainWindow):
         # Add right box
         cltRightBox = QtGui.QGridLayout()
         cltRightBox.addWidget(sigmaTitle, 1, 0)
-        cltRightBox.addWidget(sigmaSlider, 1, 1)
+        cltRightBox.addWidget(self.sigmaSlider, 1, 1)
         cltRightBox.addWidget(self.sigmaDisplay, 1, 2)
         cltRightBox.addWidget(dtTitle, 2, 0)
-        cltRightBox.addWidget(dtSlider, 2, 1)
+        cltRightBox.addWidget(self.dtSlider, 2, 1)
         cltRightBox.addWidget(self.dtDisplay, 2, 2)
         cltRightBox.addWidget(rTitle, 3, 0)
-        cltRightBox.addWidget(rSlider, 3, 1)
+        cltRightBox.addWidget(self.rSlider, 3, 1)
         cltRightBox.addWidget(self.rDisplay, 3, 2)
 
 
@@ -303,29 +303,41 @@ class Ptsgui(QtGui.QMainWindow):
 
         mainlayout.addLayout(plotBox)
         mainlayout.addLayout(cltBox)
-
-
         self.show()
 
-
-    def sigmaChangeValue(self, value):
-        smi, sma = 0, 100
-        dmi, dma = 0.001, self.max_sigma
-        self.sigma = linlin(value, smi, sma, dmi, dma)
-        self.sigmaDisplay.setText(str(self.sigma))
+    def ipChangeValue(self):
+        print self.ipDisplay.text()
 
 
-    def dtChangeValue(self, value):
-        smi, sma = 0, 100
-        dmi, dma = 0.001, 0.01
-        self.dt = linlin(value, smi, sma, dmi, dma)
-        self.dtDisplay.setText(str(self.dt))
+    def changeValue(self,value):
+        if self.sender() == self.sigmaSlider:
+            smi, sma = 0, 100
+            dmi, dma = 0.001, self.max_sigma
+            self.sigma = linlin(value, smi, sma, dmi, dma)
+            self.sigmaDisplay.setText(str(self.sigma))
+        elif self.sender() == self.dtSlider:
+            smi, sma = 0, 100
+            dmi, dma = 0.001, 0.01
+            self.dt = linlin(value, smi, sma, dmi, dma)
+            self.dtDisplay.setText(str(self.dt))
+        elif self.sender() == self.rSlider:
+            smi, sma = 0, 100
+            dmi, dma = 0.99, 1.0
+            self.r = linlin(value, smi, sma, dmi, dma)
+            self.rDisplay.setText(str(self.r))
 
-    def rChangeValue(self, value):
-        smi, sma = 0, 100
-        dmi, dma = 0.99, 1.0
-        self.r = linlin(value, smi, sma, dmi, dma)
-        self.rDisplay.setText(str(self.r))
+
+        elif self.sender() == self.dimDisplay:
+            print "dimension = " + str(value)
+        elif self.sender() == self.ncDisplay:
+            print "nc = " + str(value)
+        elif self.sender() == self.nrminDisplay:
+            print "nr min = " + str(value)
+        elif self.sender() == self.nrmaxDisplay:
+            print "nr max = " + str(value)
+        else: pass
+
+
 
     def closeEvent(self, event):
 		reply = QtGui.QMessageBox.question(self, 'Message',
