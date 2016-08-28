4973245406f1343d291ab6c32c5c45995e7bdbdb
jiajun yang <eric1986_chn@yahoo.com>
2016-08-28 18:45:31 +0200

    move listener to a separate .py

---
+++ pythonVersion/lib/Listening.py
@@ -0,0 +1,93 @@
+import numpy as np
+from lib.DSP import DSP
+import threading, OSC, socket
+
+FS = 44100/4
+
+# Hasn't tested yet.
+class Listening(object):
+    def __init__(self, gui,  ip , port=5678):
+        self.gui = gui
+        self.receive_address = ip, port
+
+    def printpara(self):
+        print self.gui.sigma
+
+
+    def trigger_handler(self, addr, tags, stuff, source):
+        print "received trigger."
+        self.sortedPos = np.array([stuff[0], stuff[1]])
+        # # Find nearest neighbour
+        # # TODO, exclude playing sound if there is no neighbouring point < dis_thres
+        # # if nearest d < dis_thres, play , else print "no neighbour"
+        #
+        deltas = self.gui.data[:, 0:2] - self.sortedPos
+        dist_2 = np.einsum('ij,ij->i', deltas, deltas)
+        idx = np.argmin(dist_2)
+        vel = np.random.rand(self.gui.dim) - 0.5  # Need to be controllable by pressure
+        pos = np.array(self.gui.data[idx, :])
+        #
+
+        trj, junk, forceSound = Trajectory.PTSM(pos, self.gui.data, vel, self.gui.exp_table,
+                                                self.gui.exp_resolution, self.gui.norm_max, sigma=self.gui.sigma,
+                                                dt=self.gui.dt, r=self.gui.r,
+                                                Nsamp=self.gui.audioVecSize, compensation=self.gui.m_comp)
+        velSound = trj[:, 0] / np.max(np.absolute(trj[:, 0])) * self.gui.windowing
+        velSound = DSP().butter_lowpass_filter(velSound, 2000.0, FS, 6)  # 6th order
+        # forceSound = forceSound / np.max(np.absolute(forceSound))
+        stopevent = threading.Event()
+        producer = threading.Thread(name="Compute audio signal", target=self.gui.proc,
+                                    args=[stopevent, velSound])
+        producer.start()
+        # Try to draw trj here. might fail though
+        self.gui.drawTrj(trj)
+
+
+    def spawn(self):
+        global  socketError
+        try:
+            self.receiveServer = OSC.OSCServer(self.receive_address)  # create a serve to receive OSC from the tablet
+            self.receiveServer.addDefaultHandlers()
+            print"Server Created."
+        except socket.error:
+            print "Socket Error."
+            socketError = True
+            print socketError
+
+    def stop_handler(self, addr, tags, stuff, source):
+        # Close the OSC server
+        print "\nClosing OSCServer."
+        self.receiveServer.close()
+        print "Waiting for Server-thread to finish"
+        try:
+            self.emorating_oscServer.join()  ##!!!
+            print "Done"
+        except AttributeError:
+            print "Done"
+
+    def add_handler(self):
+        try:
+            self.receiveServer.addMsgHandler("/trigger", self.trigger_handler)
+            self.receiveServer.addMsgHandler("/stop", self.stop_handler)
+        except AttributeError:
+            pass
+
+    def start(self):
+        try:
+
+            self.emorating_oscServer = threading.Thread(target=self.receiveServer.serve_forever)
+            self.emorating_oscServer.start()
+            print "\nOSCServer established."
+        except AttributeError:
+            pass
+
+    def stop(self):
+        # Close the OSC server
+        print "\nClosing OSCServer."
+        self.receiveServer.close()
+        print "Waiting for Server-thread to finish"
+        try:
+            self.emorating_oscServer.join()  ##!!!
+            print "Done"
+        except AttributeError:
+            print "Done"
--- pythonVersion/ptsmMain.py
+++ pythonVersion/ptsmMain.py
@@ -12,6 +12,7 @@ from lib.Exptable import Exptable
 from lib.DSP import DSP
 from lib.DataGen import DataGen
 from lib.OSCsend import OSCsend
+from lib.Listening import Listening
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
@@ -38,96 +39,6 @@ s.start()
 def linlin(x, smi, sma, dmi, dma): return (x-smi)/float(sma-smi)*(dma-dmi)+dmi
 def linlinInvert(x, smi, sma, dmi, dma): return (x-dmi)*(sma-smi)/(dma-dmi) + smi
 
-
-# Hasn't tested yet.
-class Listening(object):
-    def __init__(self, gui,  ip, port=LISTENPORT):
-        self.gui = gui
-        self.receive_address = ip, port
-
-    def printpara(self):
-        print self.gui.sigma
-
-
-    def trigger_handler(self, addr, tags, stuff, source):
-        print "received trigger."
-        self.sortedPos = np.array([stuff[0], stuff[1]])
-        # # Find nearest neighbour
-        # # TODO, exclude playing sound if there is no neighbouring point < dis_thres
-        # # if nearest d < dis_thres, play , else print "no neighbour"
-        #
-        deltas = self.gui.data[:, 0:2] - self.sortedPos
-        dist_2 = np.einsum('ij,ij->i', deltas, deltas)
-        idx = np.argmin(dist_2)
-        vel = np.random.rand(self.gui.dim) - 0.5  # Need to be controllable by pressure
-        pos = np.array(self.gui.data[idx, :])
-        #
-
-        trj, junk, forceSound = Trajectory.PTSM(pos, self.gui.data, vel, self.gui.exp_table,
-                                                self.gui.exp_resolution, self.gui.norm_max, sigma=self.gui.sigma,
-                                                dt=self.gui.dt, r=self.gui.r,
-                                                Nsamp=self.gui.audioVecSize, compensation=self.gui.m_comp)
-        velSound = trj[:, 0] / np.max(np.absolute(trj[:, 0])) * self.gui.windowing
-        velSound = DSP().butter_lowpass_filter(velSound, 2000.0, FS, 6)  # 6th order
-        # forceSound = forceSound / np.max(np.absolute(forceSound))
-        stopevent = threading.Event()
-
-        print velSound
-        producer = threading.Thread(name="Compute audio signal", target=self.gui.proc,
-                                    args=[stopevent, velSound])
-        producer.start()
-        # Try to draw trj here. might fail though
-        self.gui.drawTrj(trj)
-
-
-    def spawn(self):
-        global  socketError
-        try:
-            self.receiveServer = OSC.OSCServer(self.receive_address)  # create a serve to receive OSC from the tablet
-            self.receiveServer.addDefaultHandlers()
-            print"Server Created."
-        except socket.error:
-            print "Socket Error."
-            socketError = True
-            print socketError
-
-    def stop_handler(self, addr, tags, stuff, source):
-        # Close the OSC server
-        print "\nClosing OSCServer."
-        self.receiveServer.close()
-        print "Waiting for Server-thread to finish"
-        try:
-            self.emorating_oscServer.join()  ##!!!
-            print "Done"
-        except AttributeError:
-            print "Done"
-
-    def add_handler(self):
-        try:
-            self.receiveServer.addMsgHandler("/trigger", self.trigger_handler)
-            self.receiveServer.addMsgHandler("/stop", self.stop_handler)
-        except AttributeError:
-            pass
-
-    def start(self):
-        try:
-
-            self.emorating_oscServer = threading.Thread(target=self.receiveServer.serve_forever)
-            self.emorating_oscServer.start()
-            print "\nOSCServer established."
-        except AttributeError:
-            pass
-
-    def stop(self):
-        # Close the OSC server
-        print "\nClosing OSCServer."
-        self.receiveServer.close()
-        print "Waiting for Server-thread to finish"
-        try:
-            self.emorating_oscServer.join()  ##!!!
-            print "Done"
-        except AttributeError:
-            print "Done"
 
 
 class Ptsgui(QtGui.QMainWindow):
@@ -455,7 +366,6 @@ class Ptsgui(QtGui.QMainWindow):
             smi, sma = 0, 100
             dmi, dma = 0.001, self.max_sigma
             self.sigma = linlin(value, smi, sma, dmi, dma)
-            print self.sigma
             self.sigmaDisplay.setText(str(format(self.sigma, '.3f')))
 
         elif self.sender() == self.dtSlider:
@@ -484,10 +394,7 @@ class Ptsgui(QtGui.QMainWindow):
             print "nr max = " + str(value)
             self.genNrmax = value
         else: print "Unrecognised input source."
-
         self.androidListener.printpara()
-
-
 
     def closeEvent(self, event):
 		reply = QtGui.QMessageBox.question(self, 'Message',
@@ -498,11 +405,6 @@ class Ptsgui(QtGui.QMainWindow):
 			event.accept()
 		else:
 			event.ignore()
-
-
-
-
-
 
 def main():
 	app = QtGui.QApplication(sys.argv)
